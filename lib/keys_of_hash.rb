87da2eaba7cbfdc26dc921764581269d28ce6cb8
Kay B <kaylouisebennett@gmail.com>
2018-08-22 22:52:27 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,8 @@ class Hash
   def keys_of(*arguments)
     arr = []
      self.map do |key, value|
-      key if arguments.include?(value)
+      if arguments.include?(value)
+        
     end
   end	 
   arr
